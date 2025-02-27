90192813525c15698bb1756f9b31de9b388c1ff1
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-27 13:58:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_supabase_gold_scheme
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_supabase_gold_scheme/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_supabase_gold_scheme
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
