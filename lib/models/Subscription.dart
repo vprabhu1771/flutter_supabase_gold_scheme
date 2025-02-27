4533476769fa01bd242489920441372a242fa954
vprabhu1771 <vprabhu1771@gmail.com>
2025-02-27 18:01:20 +0530

    Update Subscription.dart

--- lib/models/Subscription.dart
+++ lib/models/Subscription.dart
@@ -3,8 +3,8 @@ import 'User.dart';
 
 class Subscription {
   final int id;
-  final User customer;
-  final GoldScheme scheme;
+  final User? customer;
+  final GoldScheme? scheme;
   final String startDate;
   final String status;
   final double totalPaid;
@@ -21,8 +21,15 @@ class Subscription {
   factory Subscription.fromJson(Map<String, dynamic> json) {
     return Subscription(
       id: json['id'],
-      customer: User.fromJson(json['customer']),
-      scheme: GoldScheme.fromJson(json['scheme']),
+      // customer: User.fromJson(json['customer']),
+      // scheme: GoldScheme.fromJson(json['scheme']),
+      customer: json['customer'] != null
+          ? User.fromJson(json['customer'])
+          : null, // Handle null customer
+
+      scheme: json['scheme'] != null
+          ? GoldScheme.fromJson(json['scheme'])
+          : null, // Handle null scheme
       startDate: json['start_date'],
       status: json['status'],
       totalPaid: double.parse(json['total_paid'].toString()),
